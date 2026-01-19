53251eedb859cb2da71a30f76297808defe8b926
Peds Yorpo <pedroyorpo17@gmail.com>
2026-01-19 21:22:25 +0800

    commit

--- app/Http/Controllers/Auth/GoogleAuthController.php
+++ app/Http/Controllers/Auth/GoogleAuthController.php
@@ -19,6 +19,35 @@ class GoogleAuthController extends Controller
 
     public function callback()
     {
+        $googleUser = Socialite::driver('google')->stateless()->user();
+
+        $user = User::where('email', $googleUser->getEmail())->first();
+
+        if (!$user) {
+            // SIGN UP
+            $user = User::create([
+                'name' => $googleUser->getName(),
+                'email' => $googleUser->getEmail(),
+                'google_id' => $googleUser->getId(),
+                'avatar' => $googleUser->getAvatar(),
+                'password' => bcrypt(Str::random(32)),
+                'email_verified_at' => now(),
+            ]);
+        }
+
+        Auth::login($user);
+
+        $token = $user->createToken('google-auth')->plainTextToken;
+
+        // 🔁 REDIRECT TO ANGULAR
+        return redirect()->away(
+            env('FRONTEND_URL') . '/auth/google/callback?token=' . $token
+        );
+    }
+
+
+    public function callbackxx()
+    {
         $googleUser = Socialite::driver('google')
             ->stateless()
             ->user();
