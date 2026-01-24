e643e495f71c3f5221dda06d70c181be81e70b63
Peds Yorpo <pedroyorpo17@gmail.com>
2026-01-24 22:28:19 +0800

    cc

--- routes/api.php
+++ routes/api.php
@@ -303,7 +303,7 @@
     Route::get('events/getEventByUuid/{uuid}', [EventController::class, 'getEventByUuid']);
 
     //PROFILE   
-    Route::post('updateProfile', [ProfilepictureController::class, 'updateProfile']);
+    Route::put('updateProfile', [ProfilepictureController::class, 'updateProfile']);
 
 });
 
